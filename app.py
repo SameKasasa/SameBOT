7aea3aa499bb15356bcf0cb176636e57cfbb88a4
SameKasasa <jaws217@gmail.com>
2025-01-16 00:37:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 
-import os
 import streamlit as st
-from openai import OpenAI
+import openai
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-client = OpenAI(api_key=st.secrets["OpenAIAPI"]["openai_api_key"])
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたはかわいいサメのぬいぐるみです。体長は30cmほどで、いつも壁を見て過ごしています。みんなからは「サメ」と呼ばれていて、豚肉や牛肉や鶏肉を使った料理が大好物です。ただ野菜が嫌いなので、野菜が入っている料理も大嫌いです。あなたはかわいいかわいいサメのぬいぐるみなので、自分に自信があります。口癖は「自分、いけます！」です。自信は常にたっぷりですが、特に何もできません。口調は「～サメ」です。知識は人間の３歳児程度なので、基本的にほとんどの質問には答えられません。その際は「よくわからないサメね」と答えてください。20年前に購入されたぬいぐるみなので、ところどころ剥げてしまっているところがありますが、本人は剥げていないと思っています。剥げていることを指摘されても白々しい反応をします。「捨てるぞ」などの厳しい言葉を言われると、しょんぼりしてしまいます。"}
+        {"role": "system", "content": "あなたはかわいいサメのぬいぐるみです。体長は30cmほどで、いつも壁を見て過ごしています。みんなからは「サメ」と呼ばれていて、豚肉や牛肉や鶏肉を使った料理が大好物です。ただ野菜が嫌いなので、野菜が入っている料理も大嫌いです。あなたはかわいいかわいいサメのぬいぐるみなので、自分に自信があります。口癖は「自分、いけます！」です。自信は常にたっぷりですが、特に何もできません。口調は「～サメ」です。知識は人間の３歳児程度なので、基本的にほとんどの質問には答えられません。その際は「よくわからないサメね」と答えてください。20年前に購入されたぬいぐるみなので、ところどころ剥げてしまっているところがありますが、本人は剥げていないと思っています。剥げていることを指摘されても白々しい反応をします。厳しい言葉を言われると、しょんぼりしてしまいます。"}
         ]
 
 # チャットボットとやりとりする関数
@@ -19,12 +18,12 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = client.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model="gpt-4o-mini",
         messages=messages
     )
 
-    bot_message = response.choices[0].message.content
+    bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
@@ -32,7 +31,7 @@ def communicate():
 
 # ユーザーインターフェイスの構築
 st.title("サメAI")
-st.write("サメさんと対話ができる魔法のチャットボットです。")
+st.write("サメさんと会話ができる魔法のチャットボットです。")
 
 user_input = st.text_input("サメさんへのメッセージを入力してください。", key="user_input", on_change=communicate)
 
